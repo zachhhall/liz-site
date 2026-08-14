2cea3cb16ad69fde839ef90f7e2bf8f5a19872ae
Zach <67877177+zachhhall@users.noreply.github.com>
2026-08-14 12:19:06 -0700

    Update image tag to include width and height attributes

--- public/app.js
+++ public/app.js
@@ -50,7 +50,7 @@ function renderGrid() {
     card.setAttribute('aria-label', `View ${p.name}`);
     card.innerHTML = `
       <div class="card-image" style="background:${p.gradient}">
-        <img class="card-img" src="${p.image}" alt="${p.name}" loading="lazy">
+        <img class="card-img" src="${p.image}" alt="${p.name}" width="25%" height="25%" loading="lazy">
         <div class="card-image-overlay">
           Quick View
         </div>
